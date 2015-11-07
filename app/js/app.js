14432c4386bb756f4d12a6a785e15b5869ce2728
Simon Jespersen <s.jespersen@stud.uis.no>
2015-11-07 20:29:58 +0100

    need api url

--- app/js/app.js
+++ app/js/app.js
@@ -11,14 +11,14 @@ angular.module('orgunitmanager', ['ui.router', 'orgunitmanager.orgList'])
     })
     .controller('homeCtrl', function ($scope, $http) {
         $scope.getContent = function () {
-            $http.get('').then(function (result) {
+            $http.get('https://apps.dhis2.org/demo/api/organisationUnits.json').then(function (result) {
                 $scope.data = [
                     { name: "tst1", content: "lorem stufstum" },
                     { name: "tst2", content: "lorem stufs2tudsadsadam" },
                     { name: "tst3", content: "lorem stuf312432442stum" }
                 ];
                 console.log(result);
-                $scope.$broadcast('dataloaded');
+                $scope.$broadcast('orgunitsloaded');
             }, function (error) {
                 console.log('error: ' + error);
             });
--- app/js/directives/orglist.js
+++ app/js/directives/orglist.js
@@ -8,8 +8,8 @@ angular.module('orgunitmanager.orgList', [])
             replace: true,
             templateUrl: "templates/directives/orglist.html",
             link: function ($scope) {
-                $scope.$on('dataloaded', function () {
-                    $timeout(function () { // You might need this timeout to be sure its run after DOM render.
+                $scope.$on('orgunitsloaded', function () {
+                    $timeout(function () {
                         angular.element('ul.tabs').tabs();
                         angular.element('.collapsible').collapsible();
                     }, 0, false);
