6ab0712e77582d66e8400bb4c9b83b90bbe550ce
Simon Jespersen <s.jespersen@stud.uis.no>
2015-11-08 02:58:53 +0100

    fills  some info in demo

--- app/js/app.js
+++ app/js/app.js
@@ -11,16 +11,13 @@ angular.module('orgunitmanager', ['ui.router', 'orgunitmanager.orgList'])
     })
     .controller('homeCtrl', function ($scope, $http) {
         $scope.getContent = function () {
+            //link fix
             $http.get('https://apps.dhis2.org/demo/api/organisationUnits.json').then(function (result) {
-                $scope.data = [
-                    { name: "tst1", content: "lorem stufstum" },
-                    { name: "tst2", content: "lorem stufs2tudsadsadam" },
-                    { name: "tst3", content: "lorem stuf312432442stum" }
-                ];
+                $scope.data = result.data.organisationUnits;
                 console.log(result);
                 $scope.$broadcast('orgunitsloaded');
             }, function (error) {
-                console.log('error: ' + error);
+                console.log(error);
             });
         }
 
