01217826bd1a38e793328adc87ac2f7328aae122
Simon Jespersen <s.jespersen@stud.uis.no>
2015-11-10 21:01:04 +0100

    working on paging

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,5 @@
 var apiBaseUrl = "";
+var pageData = {};
 
 angular.module('orgunitmanager', ['ui.router', 'orgunitmanager.orgList'])
     .config(function ($stateProvider, $urlRouterProvider) {
@@ -18,6 +19,13 @@ angular.module('orgunitmanager', ['ui.router', 'orgunitmanager.orgList'])
                 
                 $http.get(apiBaseUrl + '/organisationUnits.json').then(function (result) {
                     $scope.data = result.data.organisationUnits;
+                    pageData.page1 = result.data.organisationUnits;
+                    console.log(pageData);
+                    
+                    pageData.page2 = result.data.organisationUnits;
+                    console.log(pageData);
+                    $scope.numberOfPages = new Array(result.data.pager.pageCount);
+                    $scope.activePage = 1;
                     $scope.$broadcast('orgunitsloaded');
                 }, function (error) {
                     console.log(error);
@@ -25,8 +33,11 @@ angular.module('orgunitmanager', ['ui.router', 'orgunitmanager.orgList'])
             }, function (error) {
                 console.log(error);
             });
-
-
+        }
+        
+        $scope.goToPage = function(page) {
+            $scope.activePage = page;
+            console.log('go to page: ' + page);
         }
 
     });
