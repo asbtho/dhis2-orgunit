4531bdc1ce2d4509971b3cba51d73c7d1be71c4b
Simon Jespersen <s.jespersen@stud.uis.no>
2015-11-20 20:00:03 +0100

    work to do on edit

--- app/js/controllers/editUnitCtrl.js
+++ app/js/controllers/editUnitCtrl.js
@@ -1,8 +1,23 @@
 angular.module('orgunitmanager')
 	.controller('editUnitCtrl', ['$scope', 'orgfactory', '$stateParams', function ($scope, orgfactory, $stateParams) {
 		
+		$scope.unitToEdit = {};
+		
+		angular.element('.datepicker').pickadate({
+			selectMonths: true, // Creates a dropdown to control month
+			selectYears: 15, // Creates a dropdown of 15 years to control year
+			format: 'yyyy-mm-dd'
+		});
+		
 		$scope.initEdit = function () {
+			$scope.unitToEdit = $stateParams.unitCurrentDetails;
+			angular.element('#org_opening_date').val($scope.unitToEdit.openingDate);
+			angular.element('#org_closed_date').val($scope.unitToEdit.closedDate);
 			console.log($stateParams.unitCurrentDetails);
+		}
+		
+		$scope.editUnit = function () {
+			console.log($scope.unitToEdit);
 		}
 		
 	}]);
